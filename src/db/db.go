15c3aaebff87effa7cdeed5912523345ec530d47
alexweber7373 <alexweber7373@gmail.com>
2024-01-29 15:15:29 -0800

    database object up and running, creates stock table

---
+++ src/db/db.go
@@ -0,0 +1,52 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+//
+// 	NOTE: panic functions are used because there is no point in running the program if we cannot
+//  			store the stock information
+//
+
+// Global database variable
+var DB *sql.DB
+
+func InitDB() {
+
+	var err error
+
+	DB, err = sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Database connection failed.")
+	}
+
+	// 5 is an arbitrary number here, subject to change
+	DB.SetMaxOpenConns(5)
+	DB.SetMaxIdleConns(5)
+
+	createTables()
+
+}
+
+func createTables() {
+	createStockTable := `
+	CREATE TABLE IF NOT EXISTS stocks (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		ticker TEXT NOT NULL,
+		dateTime DATETIME NOT NULL
+	)
+	`
+
+	_, err := DB.Exec(createStockTable)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("Could not create stock table.")
+	}
+
+}
--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"example.com/stock-scraper/db"
 	"example.com/stock-scraper/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	// start the database (might crash entire program due to use of panic)
+	db.InitDB()
 
 	// Establish web scraper and gin server
 	app := gin.Default()
